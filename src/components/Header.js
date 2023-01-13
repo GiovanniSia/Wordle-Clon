cb0ac355e2caec3fd9e801cd09745d1b05dbfd63
GiovanniSia <siagivoanni1526@gmail.com>
2023-01-13 19:27:52 -0300

    Arregla responsive de GrillaPrincipal y TecladoVirtual, agrega iconos nuevos al header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,17 @@ import "../css/Header.css"
 import BotonHeader from "./BotonHeader";
 import "../css/MenuDesplegable.css";
 import ItemMenu from './ItemMenu';
+import menu from "../icons/menu.png";
+import canlendario from "../icons/calendario.png";
+import estadisticas from "../icons/estadisticas.png";
+import configuracion from "../icons/configuracion.png";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
       <div>
-        <BotonHeader onClick={() => setIsOpen(!isOpen)} key={1} src="https://img.icons8.com/ios/50/null/menu-squared-2.png" />
+        <BotonHeader onClick={() => setIsOpen(!isOpen)} key={1} src={menu} />
         {isOpen && (
           <div className="MenuDesplegable">
             {
@@ -39,7 +43,7 @@ const Header = () => {
           </div>
         )}
 
-        <BotonHeader key={2} src="https://img.icons8.com/ios/50/null/calendar--v4.png" />
+        <BotonHeader key={2} src={canlendario} />
       </div>
 
       <div>
@@ -48,9 +52,10 @@ const Header = () => {
       </div>
 
       <div>
-        <BotonHeader key={3} src="https://img.icons8.com/external-inkubators-basic-outline-inkubators/32/null/external-statistics-dashboard-ui-inkubators-basic-outline-inkubators.png" />
-        <BotonHeader key={4} src="https://img.icons8.com/material-outlined/24/null/settings--v3.png" />
+        <BotonHeader key={3} src={estadisticas} />
+        <BotonHeader key={4} src={configuracion} />
       </div>
+
 
     </header >
   );
