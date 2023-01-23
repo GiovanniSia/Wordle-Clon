1ec448ca534ad917a1fff077930f14e8d490a7b1
leonelSubelza <subelza150@gmail.com>
2023-01-23 14:43:19 -0300

    se borran palabras viejas y se agrega array de palabras nuevas

--- src/service/GeneradorDePalabra.js
+++ src/service/GeneradorDePalabra.js
@@ -1,19 +1,21 @@
-import { listaPalabras } from "./listaDePalabras.js";
+import {wordsFinal} from './WordsFinal.js'
+
+const listaPalabrasUtilizar = wordsFinal;
 
 export function generarPalabra(cantLetras) {
   let palabra = "";
   while (palabra.length !== cantLetras) {
-    palabra = listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
+    palabra = listaPalabrasUtilizar[Math.floor(Math.random() * listaPalabrasUtilizar.length)];
   }
   //se quitan las tildes y se pone en mayuscula
   return palabra.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
 }
 
 export function esPalabraValida(palabra) {
-  for (let i = 0; i < listaPalabras.length; i++) {
-    if (listaPalabras[i].normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase() === palabra) {
+  
+  for (let i = 0; i < listaPalabrasUtilizar.length; i++) {
+    if (listaPalabrasUtilizar[i].normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase() === palabra) {
       return true;
     }
   }
-  return false;
 }
