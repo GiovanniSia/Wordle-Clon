addde8baf02da87b1d4fae16e401f0974babb7d1
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2023-01-24 23:02:04 -0300

    Cambia el Como Jugar

--- src/components/ComoJugar.jsx
+++ src/components/ComoJugar.jsx
@@ -5,33 +5,32 @@ const ComoJugar = () => {
     <div className='como-jugar'>
       <div className="titulo">
         <div className="fila1">
-          <div className="letra fondo-gris">L</div>
-          <div className="letra fondo-gris">A</div>
-        </div>
-        <div className="fila2">
-          <div className="letra fondo-verde">P</div>
           <div className="letra fondo-verde">A</div>
-          <div className="letra fondo-verde">L</div>
-          <div className="letra fondo-verde">A</div>
-          <div className="letra fondo-verde">B</div>
-          <div className="letra fondo-verde">R</div>
+          <div className="letra fondo-verde">D</div>
+          <div className="letra fondo-verde">I</div>
+          <div className="letra fondo-verde">V</div>
+          <div className="letra fondo-verde">I</div>
+          <div className="letra fondo-verde">N</div>
           <div className="letra fondo-verde">A</div>
         </div>
         <div className="fila3">
           <div className="columna1">
-            <div className="letra fondo-gris">D</div>
-            <div className="letra fondo-gris">E</div>
             <div className="letra fondo-gris">L</div>
+            <div className="letra fondo-gris">A</div>
           </div>
           <div className="columna2">
-            <div className="letra fondo-amarillo">D</div>
-            <div className="letra fondo-amarillo">Í</div>
+            <div className="letra fondo-amarillo">P</div>
+            <div className="letra fondo-amarillo">A</div>
+            <div className="letra fondo-amarillo">L</div>
+            <div className="letra fondo-amarillo">A</div>
+            <div className="letra fondo-amarillo">B</div>
+            <div className="letra fondo-amarillo">R</div>
             <div className="letra fondo-amarillo">A</div>
           </div>
         </div>
       </div>
       <div className='contenido'>
-        <p className='texto-como-jugar'>El objetivo del juego es simple, adivinar la palabra oculta. La palabra tiene 5 letras y tienes 6 intentos para adivinarla. La palabra es la misma para todas las personas en ese día.</p>
+        <p className='texto-como-jugar'>El objetivo del juego es simple, adivinar la palabra oculta. La palabra tiene 5 letras y tienes 6 intentos para adivinarla. Cada vez que entres o reinicies la ventana se cambia la palabra oculta.</p>
         <p className='texto-como-jugar'>Cada intento debe ser una palabra válida. En cada ronda el juego pinta cada letra de un color indicando si esa letra se encuentra o no en la palabra y si se encuentra en la posición correcta.</p>
         <div className='tipo-colores-explicacion'>
           <div className="letra fondo-verde">D</div>
