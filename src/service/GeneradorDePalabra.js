cdb5f6ef02629f483672e2f774cb93fb1cd28413
leonelSubelza <subelza150@gmail.com>
2023-01-26 13:53:44 -0300

    Se arregla tiempo de muestra de mensajes
    Se agrega un timeOut para mostrar el mensaje emergente y luego el mensaje final

--- src/service/GeneradorDePalabra.js
+++ src/service/GeneradorDePalabra.js
@@ -1,6 +1,8 @@
 //Todas las palabras son de 5 letras, en mayúscula y sin tildes
 import {words} from './Words.js'
 
+const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ñ', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 export function generarPalabra() {
   let palabraAleatoria = words[Math.floor(Math.random() * words.length)];
   //se quitan las tildes y se pone en mayuscula
@@ -11,3 +13,7 @@ export function esPalabraValida(palabra) {
   palabra = palabra.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
   return words.includes(palabra);
 }
+
+export function esLetraValida(letra) {
+  return letras.includes(letra.toUpperCase());
+};
